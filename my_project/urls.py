6b0af0abae6e7fde240d7ce48a3e24bc93861ad1
aaronnnpf <aaronpflanner@hotmail.co.uk>
2026-05-28 14:49:56 +0100

    add sign up

--- my_project/urls.py
+++ my_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from recipe_app import views as recipe_views
 
 urlpatterns = [
+    path('recipes/', recipe_views.index, name='recipe_index'),
     path('admin/', admin.site.urls),
+    
 ]
