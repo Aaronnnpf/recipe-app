1802ac63202dc177df34a4de9f862a5830831d81
aaronnnpf <aaronpflanner@hotmail.co.uk>
2026-05-31 16:33:59 +0100

    add styling pathway to setting.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -142,7 +142,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+# Development: include project-level `styles/` directory so staticfiles finds styles.css
+STATICFILES_DIRS = [
+    BASE_DIR / 'styles',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
