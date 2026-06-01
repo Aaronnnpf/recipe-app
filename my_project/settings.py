913942d64c6501568b5b45287e4359eaf04e46ab
aaronnnpf <aaronpflanner@hotmail.co.uk>
2026-06-01 16:40:00 +0100

    add whitenoise and fix css

--- my_project/settings.py
+++ my_project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,12 +141,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Use absolute URL so static templatetag produces `/static/...` links
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-# Development: include project-level `styles/` directory so staticfiles finds styles.css
+
 STATICFILES_DIRS = [
     BASE_DIR / 'styles',
 ]
+
+# Use WhiteNoise static files storage in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
